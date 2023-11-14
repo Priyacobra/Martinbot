5c23d1c8c19c1dd33bddf445fdcecef79a37423f
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2023-11-14 10:02:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ then
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /naamartinbot
+  fi
 cd /naamartinbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
