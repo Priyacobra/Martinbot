8f746c68d131d6a359331d1fc23bb4a068849802
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2023-12-12 22:28:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mrcnu/naamartinbot.git/ naamartinbot
+  git clone https://github.com/Mrcnu/Martinbot2.git/ Martinbot2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /naamartinbot
+  git clone $UPSTREAM_REPO /Martinbot2
 fi
 cd /naamartinbot
 pip3 install -U -r requirements.txt
