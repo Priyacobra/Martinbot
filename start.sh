a9ec3fd53577948f0f382e05a04006a01c2ad71b
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2023-12-12 22:29:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mrcnu/Martinbot2.git/ Martinbot2
+  git clone https://github.com/Mrcnu/Martinbot2.git /Martinbot2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Martinbot2
