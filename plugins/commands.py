5349aa0a246c8e31ffdc7f3de95c0bf3f199b10d
KingCobra91 <117145604+KingCobra91@users.noreply.github.com>
2023-12-13 14:40:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -438,7 +438,7 @@ async def start(client, message):
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
-        f_caption = f"@AjCinemassOfcl-   {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
+        f_caption = f"@Donvijays -   {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
     if not await check_verification(client, message.from_user.id) and VERIFY == True:
         btn = [[
             InlineKeyboardButton("♻️Vᴇʀɪғʏ♻️", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
