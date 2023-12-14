ad2fc6774ad21e05fa7c7df3eb7629d80d3ae987
KingCobra91 <117145604+KingCobra91@users.noreply.github.com>
2023-12-14 10:25:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1523,7 +1523,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "shortlink_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/CnuMoviesBot")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/Don_vijay")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
