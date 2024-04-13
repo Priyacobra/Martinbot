7fd0e981011af4ffc7ea040862c5200b883f825d
Priyacobra <136266493+Priyacobra@users.noreply.github.com>
2024-04-13 08:42:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Martinbot
 fi
-cd /Martinbot
+cd /Auto-search-tamil-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
