db9dcfb4e5871181d0d2eceef59b992659038981
Priyacobra <136266493+Priyacobra@users.noreply.github.com>
2024-04-13 10:29:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Priyacobra.git /Martinbot
+  git clone https://github.com/Priyacobra/Martinbot.git /Martinbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Martinbot
